2f985b87cbb7f4958c9adcdf52f33c45843829c4
Rafal Salwa <rafal@salwa.com.pl>
2024-01-06 19:26:00 +0100

    added redirect from index route to /products

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,25 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Attribute\Cache;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Service\ProductService;
 
 
 class IndexController extends AbstractController
 {
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('product');
+    }
+
 
     #[Route('/products/{page?1}', name: 'product', defaults: ['_format' => 'html'], methods: ['GET','POST'])]
     public function productList(int $page, ProductService $productService): Response
-    {   
+    {
         //dd($productService->getAllProducts($page)); 
         return $this->render('index/index.html.twig', [
             'paginator' => $productService->getAllProducts($page),
