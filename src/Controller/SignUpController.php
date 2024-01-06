56609d4be81b854facffd191442f5d7d732c6227
Rafal Salwa <rafal@salwa.com.pl>
2024-01-06 19:31:29 +0100

    changed use import from namespace for SignupForm

--- src/Controller/SignUpController.php
+++ src/Controller/SignUpController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\SignUpType;
+use App\Form\Type\SignUpType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
